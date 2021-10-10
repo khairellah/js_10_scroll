e1a72351c8862f016134e0e16b3cbe14901d9fc4
khairellah <zakariakhairellah@gmail.com>
2021-10-10 09:46:47 +0200

    Add js code for close links

--- app.js
+++ app.js
@@ -3,14 +3,35 @@
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
-// ********** set date ************
+// 1 ********** set date ************
 const date = document.getElementById("date");
 //console.log(date);
 date.innerHTML = new Date().getFullYear();
 
-// ********** close links ************
+// 2 ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
-// ********** fixed navbar ************
+navToggle.addEventListener("click", () => {
+  /* Old Way not recommanded because it's kind of static 'if wa add another link we shold resize height in .show-links > 200px' => hadr coded 
+  linksContainer.classList.toggle("show-links");
+  */
+  // best way is get the dynamic height
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  //console.log(containerHeight);
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+    // here in the loading page if we clicked in nav btn and this size is 0 we will show the links using there dynamic height
+  } else {
+    linksContainer.style.height = 0;
+    // here when we close the links we will set the height = 0 to hide the link elements
+  }
+  // here the calculation of the height is dynamic and even if wa add another LI the height of it will add auto the the whole links using  const linksHeight = links.getBoundingClientRect().height;
+});
+
+// 3 ********** fixed navbar ************
 
 // ********** smooth scroll ************
 // select links
